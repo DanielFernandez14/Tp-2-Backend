c466cf05930b97b2e1c6a83840d2425e7cc54b8f
Daniel Matias Fernandez <daniel.mfernandez1999@gmail.com>
2025-06-30 15:42:38 -0300

    2nd B - connect MongoDb

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,1 +1,16 @@
-console.log("Probando ......")
+import express from 'express';
+import { connectMongoDb } from './config/mongo';
+
+
+process.loadEnvFile();
+
+//1234
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`✅ Servidor en escucha en el puerto http://localhost:${PORT}`)
+    connectMongoDb()
+});
+
